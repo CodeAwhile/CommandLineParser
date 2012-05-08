c93daa466b48b5de84e9518c9c318bf27dbe6df1
Craig Boucher <craig@craigeboucher.com>
2012-05-08 01:08:58 -0700

    Craig Boucher <craig.boucher@gmail.com>
    There is a failing test, but the cause is known. The main parsing method needs refactoring, but it's late.

---
+++ CommandLineParser.Tests/CommandLineParserBasicTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeAwhile.CSharp.Utilities.Test.CommandLineParserTests
+{
+    [TestClass]
+    public class CommandLineParserBasicTests
+    {
+        [TestMethod]
+        public void CanParseParameters()
+        {
+            CommandLineParser clp = new CommandLineParser(new string[] { "/" }, new string[] { ":" });
+            clp.Add<bool>("activate");
+            clp.Add<bool>("option");
+
+            string[] unParsed;
+            Dictionary<string, object> results = clp.ParseCommandLine(new string[] { "/activate:false", "skipThisParam", "/option:true" }, out unParsed);
+
+            Assert.IsTrue(results.Count == 2, @"There should only be 2 options parsed");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "activate"), @"The ""activate"" key should exist in the result");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "option"), @"The ""option"" key should exist in the result");
+            Assert.IsFalse((bool)results.Single(kvp => kvp.Key == "activate").Value, @"The ""activate"" object should be false");
+            Assert.IsTrue((bool)results.Single(kvp => kvp.Key == "option").Value, @"The ""option"" object should be true");
+            Assert.IsTrue(unParsed.Single() == "skipThisParam", @"There should be one unparsed option in the command line: skipThisParam");
+        }
+
+        [TestMethod]
+        public void CanParseConsecutiveParameters()
+        {
+            CommandLineParser clp = new CommandLineParser(new string[] { "/" }, new string[] { ":" });
+            clp.Add<bool>("activate");
+            clp.Add<bool>("option");
+
+            string[] unParsed;
+            Dictionary<string, object> results = clp.ParseCommandLine(new string[] { "/activate:false /option:true" }, out unParsed);
+
+            Assert.IsTrue(results.Count == 2, @"There should only be 2 options parsed");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "activate"), @"The ""activate"" key should exist in the result");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "option"), @"The ""option"" key should exist in the result");
+            Assert.IsFalse((bool)results.Single(kvp => kvp.Key == "activate").Value, @"The ""activate"" object should be false");
+            Assert.IsTrue((bool)results.Single(kvp => kvp.Key == "option").Value, @"The ""option"" object should be true");
+            Assert.IsFalse(unParsed.Any(), @"There should be no unparsed options in the command line");
+        }
+
+        [TestMethod]
+        public void CanParseSwitches()
+        {
+            CommandLineParser clp = new CommandLineParser(new string[] { "/" }, new string[] { ":" });
+            clp.Add<bool>("activate");
+            clp.Add<bool>("option");
+
+            string[] unParsed;
+            Dictionary<string, object> results = clp.ParseCommandLine(new string[] { "/activate:false", "/skipThisParam", "/option" }, out unParsed);
+
+            Assert.IsTrue(results.Count == 2, @"There should only be 2 options parsed");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "activate"), @"The ""activate"" key should exist in the result");
+            Assert.IsTrue(results.Any(kvp => kvp.Key == "option"), @"The ""option"" key should exist in the result");
+            Assert.IsFalse((bool)results.Single(kvp => kvp.Key == "activate").Value, @"The ""activate"" object should be false");
+            Assert.IsTrue((bool)results.Single(kvp => kvp.Key == "option").Value, @"The ""option"" object should be true");
+            Assert.IsTrue(unParsed.Single() == "/skipThisParam", @"There should be one unparsed option in the command line: /skipThisParam");
+        }
+    }
+}
--- CommandLineParser.cs
+++ CommandLineParser.cs
@@ -1,11 +1,84 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Codeawhile.CSharp.Utilities
+﻿namespace CodeAwhile.CSharp.Utilities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    // TODO:
+    // 1. aliases, e.g., -v and --verbose
+    // 2. array types
+    // 3. optional vs. required (so a validation pass on the results?)
+    // 4. usages
     public class CommandLineParser
     {
+        public CommandLineParser(IList<string> paramDelims, IList<string> valueDelims)
+        {
+            _paramDelims = paramDelims.ToArray();
+            _valueDelims = valueDelims.ToArray();
+            _parameters = new Dictionary<string, Type>();
+        }
+
+        // TODO: need an add that specifies usage
+        public void Add<T>(string paramName)
+        {
+            Add(paramName, typeof(T));
+        }
+
+        public void Add(string paramName, Type paramType)
+        {
+            _parameters.Add(paramName, paramType);
+        }
+
+        // TODO: do we want to print the usage or return it so it can be formatted?
+        public void PrintUsage()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO@crbouch: this got messy quick. Need to refactor this method.
+        // TODO@crbouch: also, this is breaking one of the tests because it's stripping off the _paramDelims of a param that's not known
+        public Dictionary<string, object> ParseCommandLine(string[] args, out string[] unparsed)
+        {
+            string cmdLine = String.Join(_defaultParamDelim, args);
+
+            var pieces = cmdLine.Split(_paramDelims, StringSplitOptions.RemoveEmptyEntries)
+                                .Select(param => param.Split(new string[] { _defaultParamDelim },
+                                                             StringSplitOptions.RemoveEmptyEntries));
+
+            unparsed = pieces.SelectMany(paramArray => paramArray.Where((param, index) => index > 0)).ToArray();
+
+            pieces = pieces.Select(paramArray => paramArray[0])
+                           .Select(param => param.Split(_valueDelims, StringSplitOptions.RemoveEmptyEntries));
+
+            unparsed = unparsed.Union(pieces.Where(paramValueArray => !_parameters.ContainsKey(paramValueArray[0])).SelectMany(paramValueArray => paramValueArray)).ToArray();
+            pieces = pieces.Where(paramValueArray => _parameters.ContainsKey(paramValueArray[0]));
+            
+            var parameters = pieces.Where(paramValueArray => paramValueArray.Length > 1);
+            var switches   = pieces.Where(paramValueArray => paramValueArray.Length == 1)
+                                   .Select(paramValueArray => new string[] { paramValueArray[0], _defaultSwitchValue });
+
+            return parameters.Union(switches)
+                             .ToDictionary(paramValueArray => paramValueArray[0],
+                                           paramValueArray => ParseToSupportedType(paramValueArray[1], _parameters[paramValueArray[0]]));
+        }
+
+        // TODO@crbouch: There's gotta be a better way than this. Reflection is hella slow.
+        //               Maybe some straight-up reflection inside a T4 or something to generate the methods... I don't know.
+        public object ParseToSupportedType(string value, Type type)
+        {
+            var meth = type.GetMethod("Parse", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            if (meth != null)
+            {
+                return meth.Invoke(null, new object[] { value });
+            }
+            return null;
+        }
+
+        protected string _defaultSwitchValue = "true";
+        protected string _defaultParamDelim = " ";
+        protected Dictionary<string, Type> _parameters;
+        protected string[] _paramDelims;
+        protected string[] _valueDelims;
     }
 }
