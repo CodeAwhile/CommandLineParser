5299c60055fb115847571adefc7aaf2a21c0a8bc
Craig Boucher <craig@craigeboucher.com>
2012-05-07 22:54:37 -0700

    Craig Boucher <craig.boucher@gmail.com>
    First commit for CommandLineParser -- setting up solution.

---
+++ CommandLineParser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codeawhile.CSharp.Utilities
+{
+    public class CommandLineParser
+    {
+    }
+}
